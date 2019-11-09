acc3d35c11bc82bc0c7f245cd965cf62627525e1
maallaki <manikanta.allaki@qvantel.com>
2019-11-09 13:47:36 +0530

    showing the analysis data

--- stockDataAnalysis/stock/urls.py
+++ stockDataAnalysis/stock/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 
-from .views import home
+from .views import *
 
 
 urlpatterns = [
     url(r'^home/$', home, name='home'),
+    url(r'stock/(?P<name>\w{0,50})/$',analysis,name='stock')
 ]
--- stockDataAnalysis/stock/views.py
+++ stockDataAnalysis/stock/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import render, redirect
 
 
 def home(request):
-    stock_names = ['REL,''HDFC', 'TCS']
+    stock_names = ['REL','HDFC','TCS']
     context = {'stock_names':stock_names}
     return render(request, "stock/home.html", context)
+
+
+def analysis(request, name):
+    stock_detilas = {'REL':{'buy':['DEC'], 'sell':['JAN'],'hold':['June']},
+                     'HDFC': {'buy': ['JAN'], 'sell': ['DEC'], 'hold': ['July']},
+                     'TCS': {'buy': ['Nov'], 'sell': ['May'], 'hold': ['Aug']}}
+    context = {'data':stock_detilas[name]}
+    return render(request, "stock/stock_analysis.html", context)
